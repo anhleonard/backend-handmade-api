4eca91f07bf545b8268a865340ab39df8a62fc02
anhleonard <anhleonard2022002@gmail.com>
2024-06-20 10:12:38 +0700

    add code

--- src/default.ts
+++ src/default.ts
@@ -2,6 +2,8 @@ export const SELLER_URL = 'https://seller-handmade-app.vercel.app';
 
 export const CLIENT_URL = 'https://client-handmade-app.vercel.app';
 
+export const ADMIN_URL = 'https://admin-handmade-app.vercel.app';
+
 export const AI_URL = 'https://850a-14-177-235-116.ngrok-free.app';
 
 // export const AI_URL = 'http://localhost:8000';
